91b87f69d676ba5cfe003ecfa95244ee52d5b15b
Sheng Qin <qinsheng32@gmail.com>
2022-10-17 00:56:08 -0700

    update main.js

---
+++ frontend/src/components/main/Main.js
@@ -0,0 +1,178 @@
+import React from "react";
+import { Route, Redirect, withRouter, Switch } from "react-router-dom";
+import { Layout, Menu, message, Image } from "antd";
+import axios from "axios";
+import utils from "../../common/Utils";
+import {
+    DatabaseOutlined,
+    SolutionOutlined,
+    UsergroupAddOutlined,
+    SettingOutlined,
+    LogoutOutlined,
+    FileWordOutlined,
+} from "@ant-design/icons";
+import store from "../../store";
+import "./Main.css";
+import main_logo from "../../images/main_logo.png";
+
+const PasswordPage = React.lazy(() => import("../password/Password"));
+const LogoutPage = React.lazy(() => import("../logout/Logout"));
+const StaffPage = React.lazy(() => import("../staff/Staff"));
+const StaffEdit = React.lazy(() => import("../staff/Edit"));
+
+const { Sider } = Layout;
+
+class Main extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentItem: [],
+        };
+
+        this.onMenuItemClick = this.onMenuItemClick.bind(this);
+        this.setCurrentItem = this.setCurrentItem.bind(this);
+        this.onClickLogout = this.onClickLogout.bind(this);
+    }
+
+    onMenuItemClick(event) {
+        this.setCurrentItem(event.key);
+        this.props.history.push(event.key);
+    }
+
+    setCurrentItem(item) {
+        let action = {
+            type: "setMenuItem",
+            value: [item],
+        };
+
+        store.dispatch(action);
+    }
+
+    setLoading(bLoading) {
+        let action = {
+            type: "setLoading",
+            value: bLoading,
+        };
+
+        store.dispatch(action);
+    }
+
+    onClickLogout() {
+        this.setLoading(true);
+
+        let self = this;
+        axios
+            .get(utils.getDomain() + "api/user/logout")
+            .then(function (res) {
+                if (
+                    0 === parseInt(res.data.code) ||
+                    1 === parseInt(res.data.code)
+                ) {
+                    self.props.history.push("/");
+                } else {
+                    message.error(res.data.message);
+                }
+                self.setLoading(false);
+            })
+            .catch(function (err) {
+                message.error(err.message);
+                self.setLoading(false);
+            });
+    }
+
+    componentDidMount() {
+        let self = this;
+        self.setState({
+            currentItem: store.getState().currentItem,
+        });
+
+        this.unsubscribe = store.subscribe(() => {
+            if (
+                self.state.currentItem.length !==
+                    store.getState().currentItem.length ||
+                self.state.currentItem[0] !== store.getState().currentItem[0]
+            ) {
+                self.setState({
+                    currentItem: store.getState().currentItem,
+                });
+            }
+        });
+    }
+
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+
+    render() {
+        return (
+            <Layout className="container">
+                <Layout>
+                    <Sider width={200} className="" style={{background : "#FFFFFF"}}>
+                        <div className="logo"  >
+                            <Image
+                                preview={false}
+                                src={main_logo}
+                                background="white"
+                            />
+                        </div>
+                        <Menu
+                            mode="inline"
+                            defaultSelectedKeys={this.state.currentItem}
+                            selectedKeys={this.state.currentItem}
+                            style={{ height: "100%", borderRight: 0}}
+                            onClick={this.onMenuItemClick}
+                            theme="light"
+                        >
+                            <Menu.Item
+                                key="/main/staff"
+                                icon={<UsergroupAddOutlined />}
+                            >
+                                Staff
+                            </Menu.Item>
+                            <Menu.Item
+                                key="/main/password"
+                                icon={<SettingOutlined />}
+                            >
+                                Password
+                            </Menu.Item>
+                            <Menu.Item
+                                key="/main/logout"
+                                icon={<LogoutOutlined />}
+                            >
+                                Logout
+                            </Menu.Item>
+                        </Menu>
+                    </Sider>
+                    <Layout style={{ padding: "24px 24px 24px" }}>
+                        <Switch>
+                            <Redirect from="/main/" to="/main/cases" exact />
+                            <Route
+                                path="/main/password"
+                                component={PasswordPage}
+                                exact
+                            />
+                            <Route
+                                path="/main/staff"
+                                component={StaffPage}
+                                exact
+                            />
+                            <Route
+                                path="/main/staff/edit/:id"
+                                component={StaffEdit}
+                                exact
+                            />
+                            <Route
+                                path="/main/logout"
+                                component={LogoutPage}
+                                exact
+                            />
+                        </Switch>
+                    </Layout>
+                </Layout>
+            </Layout>
+        );
+    }
+}
+
+export default withRouter(Main);
