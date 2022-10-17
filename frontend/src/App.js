4edc59e09759275c9a2890b17827b3f61f230e0c
Sheng Qin <qinsheng32@gmail.com>
2022-10-17 00:00:56 -0700

    react  sub/unsub components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
-import React from "react";
+import React, { Suspense } from "react";
+import {
+  HashRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import { CookiesProvider } from "react-cookie";
+import { Spin } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
+import "./App.css";
+import store from "./store";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      bLoading: false,
+    };
+  }
+
+  componentDidMount() {
+    let self = this;
+    this.unsubscribe = store.subscribe(() => {
+      self.setState({
+        bLoading: store.getState().bLoading,
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  render() {
+    return (
+      <CookiesProvider>
+        <Suspense
+          fallback={
+            <Spin
+              size="large"
+              tip="Loading..."
+              indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />}
+            >
+              <div className="App" />
+            </Spin>
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Spin
+            size="large"
+            spinning={this.state.bLoading}
+            tip="Loading..."
+            indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />}
+          >
+            <div className="App">
+              <Router>
+                <Switch>
+
+                </Switch>
+              </Router>
+            </div>
+          </Spin>
+        </Suspense>
+      </CookiesProvider>
+    );
+  }
 }
 
 export default App;
