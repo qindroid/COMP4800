cd7663cae9de85cc86473a5d0efdc2a3de106c5d
Sheng Qin <qinsheng32@gmail.com>
2022-10-17 00:55:25 -0700

    set up local test page url if in production, this need to be changed

--- frontend/src/common/Utils.js
+++ frontend/src/common/Utils.js
@@ -1,10 +1,10 @@
 let utils = {
     getDomain() {
-        
-        if (process.env.NODE_ENV === "development") {
-            return "";
-        }
-        return "https://jllss.azurewebsites.net/";
+        return "http://localhost:3000/";
+        // if (process.env.NODE_ENV === "development") {
+        //     return "";
+        // }
+        // return "https://jllss.azurewebsites.net/";
         
     },
 };
