f1cfd4a57b95edca3e9d592b9adf81a31f2f6d94
jessebadry <45022943+jessebadry@users.noreply.github.com>
2022-11-07 12:29:03 -0800

    added cashflow_create route

--- frontend/src/common/urls.js
+++ frontend/src/common/urls.js
@@ -6,9 +6,10 @@ export const USER_REGISTER_ROUTE = makeRoute("api/user/signup");
 export const USER_DETAIL_ROUTE = makeRoute("api/user/detail");
 export const USER_CREATE_ROUTE = makeRoute("api/user/create");
 export const USER_UPDATE_ROUTE = makeRoute("api/user/update");
-export const CASHFLOW_CREATE_ROUTE = makeRoute("api/cashflow/create");
 export const USER_LOGOUT_ROUTE = makeRoute("api/user/logout");
 export const USER_PASSWORD_ROUTE = makeRoute("api/user/password/");
+
+export const CASHFLOW_CREATE_ROUTE = makeRoute("api/cashflow/create");
 
 function makeRoute(url) {
   return `${utils.getDomain()}${url}`;
