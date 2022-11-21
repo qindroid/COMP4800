9e2e4dfc26cdeba01563923c9eb2255e850e550a
lliang826 <xlawrence.liangx@gmail.com>
2022-11-20 18:43:27 -0800

    Added pie chart for cash outflows

--- frontend/src/components/dashboard/Dashboard.js
+++ frontend/src/components/dashboard/Dashboard.js
@@ -6,7 +6,7 @@ import { withCookies, Cookies } from "react-cookie";
 import { instanceOf } from "prop-types";
 import store from "../../store";
 
-export const options = {
+export const barChartOptions = {
   title: "Business Cashflows",
   chartArea: { width: "50%" },
   hAxis: {
@@ -15,6 +15,16 @@ export const options = {
   vAxis: {
     title: "Company",
   },
+};
+
+export const pieChartInOptions = {
+  title: "Cash Inflow",
+  chartArea: { width: "50%" },
+};
+
+export const pieChartOutOptions = {
+  title: "Cash Outflow",
+  chartArea: { width: "50%" },
 };
 
 export const compare = (a, b) => {
@@ -44,14 +54,16 @@ export const filterValues = (array) => {
   // key = description, value = amount
   for (let i = 0; i < filteredArray.length; i++) {
     if (filteredArray[i][1] === "In") {
-      if (inMap.has(filteredArray[i][0])) { // if the key already exists
+      if (inMap.has(filteredArray[i][0])) {
+        // if the key already exists
         let value = inMap.get(filteredArray[i][0]);
         inMap.set(filteredArray[i][0], value + filteredArray[i][2]);
       } else {
         inMap.set(filteredArray[i][0], filteredArray[i][2]);
       }
     } else if (filteredArray[i][1] === "Out") {
-      if (outMap.has(filteredArray[i][0])) { // if the key already exists
+      if (outMap.has(filteredArray[i][0])) {
+        // if the key already exists
         let value = outMap.get(filteredArray[i][0]);
         outMap.set(filteredArray[i][0], value + filteredArray[i][2]);
       } else {
@@ -82,6 +94,7 @@ export const filterValues = (array) => {
 
 function Dashboard() {
   const [cashflowData, setData] = React.useState([]);
+  const [cashflowOut, setOutData] = React.useState([]);
 
   useEffect(() => {
     fetch(CASHFLOW_GLOBAL_ROUTE)
@@ -91,6 +104,13 @@ function Dashboard() {
 
         let array = filterValues(values);
         setData(array);
+
+        let newArray = JSON.parse(JSON.stringify(array)); // deep copy of the filtered array
+        newArray.forEach((i) => {
+          // remove the first column, which is the cash inflows
+          i.splice(1, 1);
+        });
+        setOutData(newArray);
       })
       .catch((error) => console.log(error));
   }, []);
@@ -98,48 +118,55 @@ function Dashboard() {
   return (
     <>
       <div>
-        <div class="">
-          <div class="grid gap-4">
-            <div class="shadow-lg col-span-2">
-              <Chart
-                chartType="BarChart"
-                width="100%"
-                height="400px"
-                data={cashflowData}
-                options={options}
-              />
-            </div>
-            <div class="bg-white shadow-lg col-span-1">
-              <Chart
-                chartType="PieChart"
-                data={cashflowData}
-                options={options}
-                width={"100%"}
-                height={"400px"}
-              />
-            </div>
-            <div class="bg-white rounded-xl shadow-lg col-span-1 text-center">
-              <h1>REVENUE</h1>
-              <h3>
-                {cashflowData.reduce((total, currentValue, currentIndex) => {
-                  if (currentIndex == 0) {
-                    return 0;
-                  } else {
-                    return +total + +currentValue[1];
-                  }
-                }, 0)}
-              </h3>
-              <h1>PROFIT</h1>
-              <h3>
-                {cashflowData.reduce((total, currentValue, currentIndex) => {
-                  if (currentIndex == 0) {
-                    return 0;
-                  } else {
-                    return +total + +currentValue[1] - +currentValue[2];
-                  }
-                }, 0)}
-              </h3>
-            </div>
+        <div class="grid grid-cols-5 gap-4">
+          <div class="shadow-lg col-span-5">
+            <Chart
+              chartType="BarChart"
+              width="100%"
+              height="400px"
+              data={cashflowData}
+              options={barChartOptions}
+            />
+          </div>
+          <div class="bg-white shadow-lg col-span-2">
+            <Chart
+              chartType="PieChart"
+              data={cashflowData}
+              options={pieChartInOptions}
+              width={"100%"}
+              height={"400px"}
+            />
+          </div>
+          <div class="bg-white shadow-lg col-span-2">
+            <Chart
+              chartType="PieChart"
+              data={cashflowOut}
+              options={pieChartOutOptions}
+              width={"100%"}
+              height={"400px"}
+            />
+          </div>
+          <div class="bg-white rounded-xl shadow-lg text-center">
+            <h1>REVENUE</h1>
+            <h3>
+              {cashflowData.reduce((total, currentValue, currentIndex) => {
+                if (currentIndex == 0) {
+                  return 0;
+                } else {
+                  return +total + +currentValue[1];
+                }
+              }, 0)}
+            </h3>
+            <h1>PROFIT</h1>
+            <h3>
+              {cashflowData.reduce((total, currentValue, currentIndex) => {
+                if (currentIndex == 0) {
+                  return 0;
+                } else {
+                  return +total + +currentValue[1] - +currentValue[2];
+                }
+              }, 0)}
+            </h3>
           </div>
         </div>
       </div>
